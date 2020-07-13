f6228b019c6a9484fa59b4fc4fb25d64a7bec78a
Carl Njoku <flavoursoft@yahoo.com>
2020-07-13 23:08:21 +0100

    Updates

---
+++ precache-manifest.4c342ef7d10659328b1213341cf261bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0874cba92a53663080cb3b964e3e13bb",
+    "url": "/longrich/:id/index.html"
+  },
+  {
+    "revision": "38dc997ee6f4a0a0d276",
+    "url": "/longrich/:id/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "611898fdd0121cb13897",
+    "url": "/longrich/:id/static/js/2.ecbcd05e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/longrich/:id/static/js/2.ecbcd05e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38dc997ee6f4a0a0d276",
+    "url": "/longrich/:id/static/js/main.816604de.chunk.js"
+  },
+  {
+    "revision": "0171a6892f37d4dfe5ae",
+    "url": "/longrich/:id/static/js/runtime-main.52826f49.js"
+  }
+]);
